3564dea2d07626824f3def830af34037d6037498
alekzandre carayon <a.p.carayon@gmail.com>
2016-05-19 23:00:46 +0200

    roles  relationship

--- app/auth/models.py
+++ app/auth/models.py
@@ -7,13 +7,14 @@ from flask.ext.login import UserMixin
 class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True, index=True)
-
+    users = db.relationship('User', backref='role')
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
 
     @property
     def password(self):
