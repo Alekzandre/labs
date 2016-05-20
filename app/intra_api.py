047aa52b1b64ecda6c5bbcd3dd9ad790012cba0a
Sebastien BENOIT <sb@42.fr>
2016-05-20 17:34:06 +0200

    intra_api token

--- app/intra_api.py
+++ app/intra_api.py
@@ -5,6 +5,7 @@ class IntraApi:
 	def __init__(self, client_id, client_secret):
 		self.client_id = client_id
 		self.client_secret = client_secret
+		self.token = ''
 
 	def get_token(self):
 		url = "https://api.intra.42.fr/oauth/token"
