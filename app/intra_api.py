1a6862547a50326f2bc99365f2235073ed4b5a2e
alekzandre carayon <a.p.carayon@gmail.com>
2016-05-24 11:04:05 +0200

    mdif intra_api

--- app/intra_api.py
+++ app/intra_api.py
@@ -48,3 +48,42 @@ class IntraApi:
                 elem1 = [elem['id'], elem['name'], elem['description']]
                 projects.append(elem1)
         return projects
+
+    def create_User(self, mail, fname, lname, campus_id='1', cursus_id='1'):
+        self.get_cursus()
+        url = "https://api.intra.42.fr/v2/users"
+        data = {}
+        data['access_token'] = self.token
+        data['email'] = mail
+        data['first_name'] = fname
+        data['last_name'] = lname
+        data['campus_id'] = campus_id
+        data['cursus_id'] = cursus_id
+        values = urllib.urlencode(data)
+        req = urllib2.Request(url, values)
+        response = urllib2.urlopen(req)
+        return response
+
+    def subscribe_User_To_Cursus(self, user_id, cursus_id='1'):
+        self.get_cursus()
+        url = "https://api.intra.42.fr/v2/cursus_users"
+        data = {}
+        data['access_token'] = self.token
+        data['cursus_id'] = cursus_id
+        data['user_id'] = user_id
+        values = urllib.urlencode(data)
+        req = urllib2.Request(url, values)
+        response = urllib2.urlopen(req)
+        return response
+
+    def subscribe_User_To_Project(self, project_id, user_id):
+        self.get_cursus()
+        url = "https://api.intra.42.fr/v2/" + project_id + "/projects_users"
+        data = {}
+        data['access_token'] = self.token
+        data['user_id'] = user_id
+        values = urllib.urlencode(data)
+        req = urllib2.Request(url, values)
+        response = urllib2.urlopen(req)
+        return response
+
