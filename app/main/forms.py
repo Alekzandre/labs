2bfa16692a00822ba7e3c7f4d86f0af8e86069c5
alekzandre carayon <a.p.carayon@gmail.com>
2016-05-24 13:16:01 +0200

    update

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,6 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField, FileField
-from wtforms.validators import Required
+from wtforms import StringField, SubmitField, FileField, PasswordField
+from wtforms.validators import Required, EqualTo
 from wtforms import validators
 import re
 
@@ -11,4 +11,10 @@ class NameForm(Form):
 
 class PicUploadForm(Form):
     image = FileField(u'Image File')
+    submit = SubmitField('got it')
+
+class UpdateUserForm(Form):
+    password = PasswordField('New Password', validators=[
+        Required(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm New Password', validators=[Required()])
     submit = SubmitField('got it')
--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template, session, redirect, url_for, flash, request
 from flask.ext.login import login_required
 from . import main
 from .. import db
-from .forms import PicUploadForm
+from .forms import PicUploadForm, UpdateUserForm
 from ..firm.models import Firm
 from ..auth.models import User, Role
 import os
@@ -21,6 +21,18 @@ def index():
 def user(user_id):
     user = User.query.get(user_id)
     return render_template('main/user.html', user=user)
+
+@main.route('/user/update/<int:user_id>', methods=['GET', 'POST'])
+@login_required
+def update_profile(user_id):
+    user = User.query.get(user_id)
+    form = UpdateUserForm()
+    if form.validate_on_submit():
+        user.password=form.password.data
+        print form.password.data
+        db.session.commit()
+        return redirect(url_for('main.user', user_id=user_id))
+    return render_template('main/update.html', form=form)
 
 
 @main.route('/upload', methods=['GET', 'POST'])
