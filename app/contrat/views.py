1e744b47e09ba7d38e099824f91af50537980a20
alekzandre carayon <a.p.carayon@gmail.com>
2016-06-04 18:13:27 +0200

    ajout de remove_user

--- app/contrat/views.py
+++ app/contrat/views.py
@@ -2,11 +2,12 @@ from flask import render_template, session, redirect, url_for, flash, request
 from flask.ext.login import login_user, logout_user, login_required
 from . import contrat
 from .models import Contrat
+from ..auth.models import User
 from .forms import ContratForm, UpdateContratForm
 from .. import db
 
+
 def registration_already_exist(user, contrat):
-    print "i m there"
     for elem in user.contrat.all():
         print elem.id
         if elem.id == contrat.id:
@@ -60,3 +61,17 @@ def create_contrat():
         db.session.add(contrat)
         return redirect(url_for('contrat.index'))
     return render_template('contrat/create_contrat.html', form=form)
+
+
+@contrat.route('/remove/user/<int:user_id>/<int:contrat_id>', methods=['GET', 'POST'])
+@login_required
+def remove_user(user_id, contrat_id):
+    contrat = Contrat.query.get_or_404(contrat_id)
+    user = User.query.get_or_404(user_id)
+    if registration_already_exist(user, contrat) == 1:
+        user.contrat.remove(contrat)
+        db.session.commit()
+        contrat.used_slot -= 1
+        return redirect(url_for('contrat.index'))
+    flash("Ce user n'est pas inscrit a cette formation")
+    return redirect(url_for('contrat.index'))
